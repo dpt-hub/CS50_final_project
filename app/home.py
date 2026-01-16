781a17d1431afa4f1786ea8ace9ddf5a3943f06c
Guilherme Bento <guilhermebentodpt@gmail.com>
2026-01-16 11:27:26 +0000

    Can navigate to other pages through navbar (base.html)

--- app/home.py
+++ app/home.py
@@ -1,25 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 
 bp = Blueprint('home', __name__)
 
 @bp.route('/')
 def homepage():
-    return render_template('landing_page.html')
+    return render_template('home/landing_page.html')
 
 @bp.route('/features')
 def features():
 
     # TODO: Create html file
-    return render_template('features.html')
+    return render_template('home/features.html')
 
 @bp.route('/pricing')
 def pricing():
 
     # TODO: Create html file
-    return render_template('pricing.html')
+    return render_template('home/pricing.html')
 
 @bp.route('/about-us')
 def about_us():
 
     # TODO: Create html file
-    return render_template('about_us.html')
+    return render_template('home/about_us.html')
