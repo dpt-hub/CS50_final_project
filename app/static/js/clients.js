30dfbc8a312991362e919e3e2de75d46c13d6034
Guilherme Bento <guilhermebentodpt@gmail.com>
2026-01-21 11:33:57 +0000

    Changed sorting table logic in (clients.js)

--- app/static/js/clients.js
+++ app/static/js/clients.js
@@ -1,36 +1,79 @@
-let sortTable = (n) => {
-    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("clientTable");
-    switching = true;
-    dir = "asc";
-    while (switching) {
-        switching = false;
-        rows = table.rows;
-        for (i = 1; i < (rows.length - 1); i++) {
-        shouldSwitch = false;
-        x = rows[i].getElementsByTagName("td")[n];
-        y = rows[i + 1].getElementsByTagName("td")[n];
-        if (dir == "asc") {
-            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            shouldSwitch = true;
-            break;
+// Sorting Table when clicking the column headers
+let sortData = () => {
+    // Select table in javascript
+    let table = document.querySelector("#clientTable")
+    // Select the column header row
+    let columnRow = table.rows[0]
+    for(let i = 1; i < columnRow.childElementCount; i++)
+    {
+        let sortingDirection = "ascending"
+        console.log(table.rows.length)
+        columnRow.children[i].addEventListener("click", () => {
+            // Do the algorithm until all data is sorted
+            
+            let stillSorting = true;
+            while(stillSorting)
+            {
+                stillSorting = false;
+                for(let j = 1; j < table.rows.length - 1; j++)
+                {
+                    // Select a row of data
+                    let rowOne = table.rows[j]
+                    // Select the row after it
+                    let rowTwo = table.rows[j + 1]
+                    // Check if the first row of data's cell corresponding column header clicked is bigger than the second row's data cell
+                    if (rowOne.children[i].innerText.toLowerCase() > rowTwo.children[i].innerText.toLowerCase() && sortingDirection == "ascending")
+                    {
+                        rowOne.parentNode.insertBefore(rowTwo, rowOne)
+                        stillSorting = true;
+                    }
+                    else if (rowOne.children[i].innerText.toLowerCase() < rowTwo.children[i].innerText.toLowerCase() && sortingDirection == "descending")
+                    {
+                        rowOne.parentNode.insertBefore(rowTwo, rowOne)
+                        stillSorting = true;
+                    }
+                }
             }
-        } else if (dir == "desc") {
-            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            shouldSwitch = true;
-            break;
+            if (sortingDirection == "ascending") 
+            {
+                sortingDirection = "descending"
             }
-        }
-        }
-        if (shouldSwitch) {
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-        switchcount ++;
-        } else {
-        if (switchcount == 0 && dir == "asc") {
-            dir = "desc";
-            switching = true;
-        }
-        }
+            else 
+            {
+                sortingDirection = "ascending"
+            }            
+        })
     }
 }
+
+sortData()
+    
+    
+        // When column header data is clicked sort the table according to a sorting function
+        // Make sure to change the innerText of the table header selected to reflect an arrow up or down (sorting feedback)
+    // Sorting Algorithm
+
+let createClient = document.querySelector('#createClient')
+
+createClient.addEventListener("click", () => {
+    createClient.remove();
+    let table = document.getElementById("clientTable");
+    
+    // Create a Form element that surrounds the table - method "POST"
+
+    // Insert a new row and it's respective cells
+
+    // Submit button to the form
+
+    // Input type text - required
+    
+    let newRow = table.insertRow();
+    let newForm = newRow.appendChild(document.createElement("FORM"))
+    let confirmCell = newForm.insertCell(0)
+    confirmCell.appendChild(document.createElement("INPUT"))
+    let nameCell = newRow.insertCell(1)
+    let typeCell = newRow.insertCell(2)
+    let latCell = newRow.insertCell(3)
+    let lonCell = newRow.insertCell(4)
+}
+)
