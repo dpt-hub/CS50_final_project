1ab6193d5d7e5a06e02487bc177efebebeadeb9f
Guilherme Bento <guilhermebentodpt@gmail.com>
2026-01-23 19:23:48 +0000

    Python parses SQLite table and detects types (db.py)

--- app/db.py
+++ app/db.py
@@ -4,7 +4,10 @@ from flask import g, current_app
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(current_app.config['DATABASE'])
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
 
         # Makes it possible to access data using named indexes (e.g row["name"] fetches data "name" key)
         g.db.row_factory = sqlite3.Row
