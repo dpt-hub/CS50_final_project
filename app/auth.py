8e79e312ca331897ee42b91948fb9e62b85d129c
Guilherme Bento <guilhermebentodpt@gmail.com>
2026-01-23 11:38:36 +0000

    Fixes spacing (auth.py) + password requirements in (register.html)

--- app/auth.py
+++ app/auth.py
@@ -6,7 +6,6 @@ import re
 
 bp = Blueprint('auth', __name__, url_prefix='/auth' )
 
-
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
 
@@ -52,7 +51,6 @@ def register():
         return render_template('auth/register.html')
             
 
-
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
 
@@ -86,6 +84,7 @@ def login():
     
     return render_template('auth/login.html')
 
+
 @bp.before_app_request
 def load_user():
     user_id = session.get('user_id')
@@ -95,10 +94,12 @@ def load_user():
     else:
         g.user = get_db().execute('SELECT * FROM users WHERE user_id = ?', (user_id,)).fetchone()
 
+
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('auth.login'))
+
 
 def login_required(view):
     @functools.wraps(view)
